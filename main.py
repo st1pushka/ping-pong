be51aea5fae4b404f67dd1025891a3cb4ce9bfc3
st1pushka <stkvach@gmail.com>
2026-04-26 13:23:40 +0300

    Game loop main.py
    Создание игрового окна. Обработка события выхода из игры. Установка частоты кадров

--- main.py
+++ main.py
@@ -34,3 +34,15 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption("Ping-Pong")
 background = transform.scale(image.load("tennis-court.webp"), (win_width, win_height))
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False 
+
+    display.update()
+    clock.tick(FPS)
